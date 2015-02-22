c521b9acfbb2b561ef9762f2023dd960de5911e9
gabi <gabi@gabi-MS-1738>
2015-02-22 09:17:48 +0200

    Removed unnecessary elements

--- kform.js
+++ kform.js
@@ -6,24 +6,26 @@
             return false;
         }
         
-        if(!$(handler).hasClass('kform')){
-            $(handler).addClass('kform');
-        }
-        
-        $(handler).attr('data-type','json');
-        
         if(config !== undefined && typeof config.onInit === 'function'){
             config.onInit(handler);
         }
         
-        $('body').on('submit',handler,function(e){
-            e.stopPropagation();
+        $(document).on('submit',handler,function(e){
+            e.preventDefault();
             
             var form = $(this);
+            
+            $(form).attr('data-type','json');
+            
+            if(config !== undefined && typeof config.onSubmit === 'function'){
+                config.onSubmit(form);
+            }
+            
             var action = $(form).attr('action');
             var data = $(form).serialize();
             var method = $(form).attr('method');
             var data_type = $(form).attr('data-type');
+            
             
             var submitbtn = $(form).find('button.submit-btn');
             if($(submitbtn).attr('data-loading-text') === undefined || $(submitbtn).attr('data-loading-text') === ''){
@@ -41,7 +43,6 @@
                 data: data,
                 success: function(data){
                     if(data.success === false){
-                        regenerateCSRF(form);
                         $.each(data.errors,function(field,item){
                             var formgroup = $(form).find('.form-group.'+field);
                             if($(formgroup).is('*')){
@@ -84,17 +85,13 @@
                         $(submitbtn).button('reset');
                         cb($(form).attr('data-callback-error'),form,data);
                         
-                        if($(form).hasClass('loaded-as-modal')){
-                            performAsModal(form,data,submitbtn);
-                            return false;
-                        }
-                    }
-                    else{
-                        if($(form).hasClass('loaded-as-modal')){
-                            performAsModal(form,data,submitbtn);
-                            return false;
+                        if(data.message){
+                            messageNotification(form,data,submitbtn);
+                            $(submitbtn).button('reset');
                         }
                         
+                    }
+                    else{
                         doAfterSuccess(form,data,submitbtn);
                     }
                     
@@ -109,85 +106,6 @@
             return false;
         });
         
-        
-        $('body').on('click','.kform-btn-save',function(e){
-            e.stopPropagation();
-            var form = $(this).parent().parent().find(handler);
-            
-            var modalFooter = $(this).closest('.modal-footer');
-            $(modalFooter).find('.kform-btn').addClass('hide');
-            $(modalFooter).append('<div class="action-loading">'+$(modalFooter).attr('data-loading-text')+'</div>');
-            
-            $(form).submit();
-            return false;
-        });
-
-        $('body').on('click','.kform-btn-cancel',function(e){
-            e.stopPropagation();
-            var form = $(this).parent().parent().find(handler);
-            $(form).find('.form-group.has-error').removeClass('has-error');
-            $(form).find('p.error-message').remove();
-            destroyModal();
-            return false;
-        });
-        
-        $('body').on('hide.bs.modal','.modal-kform', function (e) {
-            setTimeout(function() {
-                $(".modal-kform").remove();
-                $(".modal-backdrop").remove();
-            }, 500);
-        })
-        
-        $('body').on('click','.kmodal',function(e){
-            //e.stopPropagation();
-            var obj = $(this);
-            var jsonData = ($(obj).attr('data-form-json') !== undefined ? $(obj).attr('data-form-json') : {});
-            jsonData['data_target'] = $(obj).attr('data-target');
-            $.ajax({
-                type: "POST",
-                url: $(obj).attr('data-form'),
-                dataType: 'json',
-                data: jsonData,
-                success: function(data){
-                    $('body').append(data.response.form);
-                    var loadedForm = $('.modal-kform').find('form');
-                    $(loadedForm).addClass('loaded-as-modal');
-                    $(loadedForm).append('<input type="hidden" name="is_modal" value="1">');
-                    var tg = '#'+$(obj).attr('data-target');
-                    $(tg).modal('show');
-                    
-                    if ( $.isFunction($.fn.select2) ) {
-                        $(tg).find(".sel2").select2({
-                            allowClear: true
-                        });
-
-                        $(tg).find(".sel2").css('min-width','168px');
-
-                        $(tg).find('body').on('blur','.select2-input',function(e){
-                            $(this).css('width','auto');
-                        });
-                    }
-                    
-                    if ( $.isFunction($.fn.autosize) ) {
-                        if($(".autosize").is('*')){
-                            $(".autosize").autosize({append: "\n"});
-                        }
-                    }
-                    
-                    $($(obj).attr('data-target')).on('hidden.bs.modal', function (e) {
-                        if ( $.isFunction($.fn.select2) ) {
-                            $(tg).find('.sel2').select2('destroy');
-                        }
-                        destroyModal();
-                    });
-                },
-                error: function(e){
-                    console.log(e.responseText);
-                }
-            });
-            //return false;
-        });
-        
         var cb = function(callback,form,data){
             if(callback !== undefined){
                 var fn = window[callback];
@@ -199,7 +117,7 @@
             return false;
         };
         
-        var messageNotification = function(form,data){
+        var messageNotification = function(form,data,submitbtn){
             $(form).find(".notification-message").remove();
             var delay = (data.message.delay !== undefined ? data.message.delay : 3000);
             var notificationHolder = (data.message.holder !== undefined ? data.message.holder : '.notification-holder');
@@ -232,8 +150,17 @@
             else{
                 $(form).find(notificationHolder).html('<div class="notification-message">'+data.message+'</div>');
             }
+            
+            if(data.message.redirect_to !== undefined){
+                submitbtn.remove();
+            }
+            
             $(form).find(".notification-message").delay(delay).fadeOut('slow',function(){
                 $(this).remove();
+                if(data.message.redirect_to !== undefined){
+                    window.location.href = data.message.redirect_to;
+                    return false;
+                }
             });
         }
         
@@ -255,46 +182,6 @@
             $.each(items, function (item, message) {
                 $('body').find(item).html(message);
             });
-        };
-        
-        var performAsModal = function(form,data,submitbtn){
-            doAfterSuccess(form,data,submitbtn);
-            
-            var delay = (data.success === true && data.message.delay !== undefined ? data.message.delay : 0);
-            
-            if(delay > 0){
-                setTimeout(function() {
-                    if(data.success === true){
-                        if(data.message.redirect_to_after_delay !== undefined){
-                            window.location.href = data.message.redirect_to_after_delay;
-                            return false;
-                        }
-
-                        if(data.message.close_modal_after_delay !== undefined){
-                            destroyModal();
-                        }
-                    }
-                    var modalFooter = $(form).parent().parent().find('.modal-footer');
-                    $(modalFooter).find('.kform-btn').removeClass('hide');
-                    $(modalFooter).find('.action-loading').remove();
-                }, delay);
-            }
-            else{
-                if(data.success === true){
-                    if(data.message.redirect_to_after_delay !== undefined){
-                        window.location.href = data.message.redirect_to_after_delay;
-                        return false;
-                    }
-
-                    if(data.message.close_modal_after_delay !== undefined){
-                        destroyModal();
-                    }
-                }
-                var modalFooter = $(form).parent().parent().find('.modal-footer');
-                $(modalFooter).find('.kform-btn').removeClass('hide');
-                $(modalFooter).find('.action-loading').remove();
-            }
-            
         };
         
         var doAfterSuccess = function(form, data, submitbtn){
@@ -319,7 +206,7 @@
             }
 
             if(data.message){
-                messageNotification(form,data);
+                messageNotification(form,data,submitbtn);
                 $(submitbtn).button('reset');
             }
 
@@ -335,42 +222,10 @@
             return true;
         };
         
-        var destroyModal = function(doHide){
-            $('.modal-kform').modal('hide');
-            setTimeout(function() {
-                $(".modal-kform").remove();
-                $(".modal-backdrop").remove();
-            }, 500);
-        }
-        
         function resetForm($form) {
             $form.find('input:text, input:password, input:file, select, textarea').val('');
             $form.find('input:radio, input:checkbox').removeAttr('checked').removeAttr('selected');
-            if ( $.isFunction($.fn.select2) ) {
-                $form.find('.sel2').select2('val','');
-            }
         }
         
-        function regenerateCSRF(form){
-            var csrf_name = null;
-            if($(form).find('input[name="csrf"]').is('*')){
-                var csrf = $(form).find('input[name="csrf"]');
-                csrf_name = ($(csrf).attr('data-name') !== undefined ? $(csrf).attr('data-name') : 'default');
-            }
-            
-            if(csrf_name !== null){
-                $.ajax({
-                    type: "GET",
-                    url: route('csrf',{name: csrf_name}),
-                    dataType: 'json',
-                    success: function(data){
-                        $(csrf).val(data.response.csrf);
-                    },
-                    error: function(e){
-                        console.log(e.responseText);
-                    }
-                });
-            }
-        }
     };
 })( jQuery );
